43731896ba61b39623ef934a3abab8dee1aaad96
FelipeUTFPR <fps2887@gmail.com>
2021-10-27 18:52:25 -0300

    1º commit

---
+++ atividade6.js
@@ -0,0 +1,50 @@
+// Employees Object List
+employees = [
+    {
+      id: 11,
+      name: "joao da silva",
+      cpf: "111.222.333-44",
+      category: "tecnico",
+    },
+    {
+      id: 22,
+      name: "joana da silva",
+      cpf: "111.222.333-55",
+      category: "tecnico",
+    },
+    {
+      id: 23,
+      name: "joana da Mata",
+      cpf: "111.222.333-66",
+      category: "analista",
+    },
+    {
+      id: 24,
+      name: "joana da Mata",
+      cpf: "23456789-00",
+      category: "gerente",
+    },
+    {
+      id: 45,
+      name: "joana da Mata",
+      cpf: "111222333-44",
+      category: "gerente",
+    },
+    {
+      id: 36,
+      name: "joana da Mata",
+      cpf: "654321987-23",
+      category: "gerente",
+    },
+    {
+      id: 27,
+      name: "joana da Mata",
+      cpf: "098876654-99",
+      category: "gerente",
+    },
+  ];
+  
+  //retorna apenas os gerentes
+  gerentes = employees.filter((employee) => employee.category === "gerente");
+  console.log(gerentes);
+  
